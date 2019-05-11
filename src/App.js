8a73ef23bc25231489a1fb1964c610ad2498989f
alon6699 <allonmaliii66@gmail.com>
2019-05-11 19:51:49 +0300

    add bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
+import {Container, Row, Col} from 'react-bootstrap';
 
 class App extends Component {
   render() {
       return (
-        <div>
-        sa
-        </div>
+<Container>
+  <Row>
+    <Col>1 of 2</Col>
+    <Col>2 of 2</Col>
+  </Row>
+  <Row>
+    <Col>1 of 3</Col>
+    <Col>2 of 3</Col>
+    <Col>3 of 3</Col>
+  </Row>
+</Container>
       );
     }
   }
