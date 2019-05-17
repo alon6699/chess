6080a3817ae345731f0faca17dc8d1cc3c05a15d
alon6699 <allonmaliii66@gmail.com>
2019-05-17 21:55:57 +0300

    Create home page. Add routing to game, option and back home

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import { Container, Row } from 'react-bootstrap';
 import Game from './Game/Game';
+import Options from './Options/Options';
+import Home from './Home/Home';
+import { Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
+      <header>
       <Switch>
-      <Route exact path='/' component={Game}/>
-      {/* <Route path='/roster' component={Roster}/>
-      <Route path='/schedule' component={Schedule}/> */}
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/game' component={Game}/>
+      <Route path='/options' component={Options}/> */}
     </Switch>
+    </header>
+
       // <Container>
       //   <Row className="justify-content-md-center">
       //   play
